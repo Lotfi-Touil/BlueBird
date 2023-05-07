661a589393a7e89a3084fed505e8ec866afc0513
Lotfi-Touil <lotfi.touil7@gmail.com>
2023-05-07 15:30:13 +0200

    feat : ajout de bootstrap

--- www/Controllers/Auth.php
+++ www/Controllers/Auth.php
@@ -8,16 +8,19 @@ class Auth{
     public function login(): void
     {
         $view = new View('Auth/login', 'back');
+        $view->assign('title', 'Blue Bird | Connexion');
     }
 
     public function logout(): void
     {
         $view = new View('Auth/logout', 'back');
+        $view->assign('title', 'Blue Bird | Déconnexion');
     }
 
     public function register(): void
     {
         $view = new View('Auth/register', 'back');
+        $view->assign('title', 'Blue Bird | Inscription');
     }
 
 }
--- www/Views/back.tpl.php
+++ www/Views/back.tpl.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title><?= $title ?? 'Blue bird' ?></title>
-    <meta name="description" content="Ceci est ma super page">
+    <meta name="description" content="<?= $description ?? 'Découvrez Blue Bird, une plateforme géniale !' ?>">
+    <link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
     <h1>Template Back</h1>
 
     <?php include $this->view; ?>
 
+    <script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
