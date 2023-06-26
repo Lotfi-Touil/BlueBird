5c74ed570ad284831b81a3a039a21c90440b0309
PHANGWilly <pro.phang.willly@gmail.com>
2023-06-26 18:53:26 +0200

    feat : home page template

---
+++ www/Views/Partials/home/film-of-the-moment.partial.php
@@ -0,0 +1,34 @@
+<!-- START OF FILM OF THE MOMENT -->
+<section class="film-of-the-moment py-2">
+    <h2 class="mb-4">Le film du moment</h2>
+    <div class="row sm-gap-4">
+        <div class="col-lg-6">
+            <div class="">
+                <img src="https://wallpaperaccess.com/full/8652624.jpg" alt="" class="ratio ratio-16x9 w-100 h-100 rounded-4 object-fit-cover">
+            </div>
+        </div>
+        <div class="col-lg-6 w-100 h-100">
+            <div class="d-flex flex-column justify-content-between gap-2">
+                <h3>Titre du film</h3>
+                <div class="d-flex gap-2 flex-column">
+                    <div class="tags d-flex gap-2 align-items-center overflow-auto" style="white-space: nowrap;">
+                        <a class="px-2 py-1 text-primary text-decoration-none">Tags : </a>
+                        <!--SELECT RANDOM TAGS-->
+                        <a href="#" class="tag px-2 py-1 bg-primary text-white rounded-2 text-decoration-none">Marvel</a>
+                        <a href="#" class="tag px-2 py-1 bg-primary text-white rounded-2 text-decoration-none">Action</a>
+                        <a href="#" class="tag px-2 py-1 bg-primary text-white rounded-2 text-decoration-none">Spider-Man</a>
+                        <a href="#" class="tag px-2 py-1 bg-primary text-white rounded-2 text-decoration-none">MCU</a>
+                    </div> 
+                </div>
+                <div class="">
+                    <!--RESUME-->
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque amet neque voluptates nesciunt, ut quisquam a magni fugit! Assumenda atque cupiditate omnis consequatur recusandae voluptatibus mollitia neque labore blanditiis aspernatur delectus in, illo quaerat eveniet. Provident eius minus veritatis asperiores.
+                    </p>
+                </div>
+                <a href="#" class="bg-primary text-white rounded-2 text-decoration-none py-1 px-4 w-50">En Savoir Plus</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END OF FILM OF THE MOMENT -->
