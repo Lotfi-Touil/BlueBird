f490492027f9a7bc1bcb370157e1619402ac6ddc
PHANGWilly <pro.phang.willly@gmail.com>
2023-06-28 11:13:58 +0200

    modify home page

--- www/Views/Main/home.view.php
+++ www/Views/Main/home.view.php
@@ -1,5 +1,10 @@
+<?php $this->partial('home/easy-search'); ?>
 <?php $this->partial('home/film-of-the-moment'); ?>
+<?php $this->partial('home/separator'); ?>
 <?php $this->partial('home/latest-releases'); ?>
+<?php $this->partial('home/separator'); ?>
 <?php $this->partial('home/top-films'); ?>
+<?php $this->partial('home/separator'); ?>
 <?php $this->partial('home/top-studios'); ?>
+<?php $this->partial('home/separator'); ?>
 <?php $this->partial('home/top-actors'); ?>
