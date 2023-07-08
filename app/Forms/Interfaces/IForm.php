2a7968aa8ad64b5a1b9b06485f0e7d83e8eafa2c
Lotfi-Touil <lotfi.touil7@gmail.com>
2023-07-09 01:11:51 +0200

    CODE REVIEW

--- app/Forms/Interfaces/IForm.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Forms\Interfaces;
-
-interface IForm
-{
-    public function getConfig(): array;
-    public function getMethod(): string;
-    public function isSubmit(): bool;
-}
--- app/Views/message/back/show.view.php
+++ app/Views/message/back/show.view.php
@@ -1,6 +1,6 @@
 <div class="card w-100 mb-3">
     <div class="card-header d-flex">
-        <h4 class="card-title">Détails du Message #<?= $message['message_id'] ?></h4>
+        <h4 class="card-title">Détails du Message #<?= $message['id'] ?></h4>
         <a href="/admin/message/list" class="btn btn-primary ml-auto">
             <span class="icon text-white-50">
                 <i class="fa fa-arrow-left"></i>
@@ -12,30 +12,28 @@
         <div class="w-75">
             <form>
                 <div class="form-group">
-                    <label for="object">Object</label>
-                    <input type="text" id="object" name="object" class="form-control" value="<?= $message['object'] ?>" readonly>
+                    <label for="object">Objet</label>
+                    <input type="text" class="form-control" value="<?= $message['object'] ?>" readonly>
                 </div>
                 <div class="form-group">
                     <label for="message">Message</label>
-                    <textarea id="message" name="message" class="form-control" readonly><?= $message['message'] ?></textarea>
+                    <textarea class="form-control" readonly><?= $message['message'] ?></textarea>
                 </div>
                 <div class="form-group">
-                    <label for="firstname">Firstname</label>
-                    <input id="firstname" name="firstname" class="form-control" readonly value="<?= $message['firstname'] ?>">
+                    <label for="firstname">Prénom</label>
+                    <input class="form-control" readonly value="<?= $message['firstname'] ?>">
                 </div>
                 <div class="form-group">
-                    <label for="lastname">Lastname</label>
-                    <input id="lastname" name="lastname" class="form-control" readonly value="<?= $message['lastname'] ?>">
+                    <label for="lastname">Nom</label>
+                    <input class="form-control" readonly value="<?= $message['lastname'] ?>">
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input id="email" name="email" class="form-control" readonly value="<?= $message['email'] ?>">
+                    <input class="form-control" readonly value="<?= $message['email'] ?>">
                 </div>
                 <div class="form-group">
-                    <label for="categorie">Catégorie</label>
-                    <select id="categorie" name="categorie" class="form-control" readonly>
-                        <option value="<?= $message['categorie_message_id'] ?>" readonly><?= $message['description'] ?></option>
-                    </select>
+                    <label for="category">Catégorie</label>
+                    <input type="text" class="form-control" readonly value="<?= $message['description'] ?>">
                 </div>
             </form>
         </div>
