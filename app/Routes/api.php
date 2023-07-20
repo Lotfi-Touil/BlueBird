d634b89654b4848299c84a1db508264070ff722d
Matiss Haillouy <mhaillouy@myges.fr>
2023-07-20 21:46:52 +0200

    feat : step4

--- app/Routes/api.php
+++ app/Routes/api.php
@@ -3,6 +3,7 @@
 use App\Controllers\Api\StartSetupController;
 use App\Controllers\Api\DatabaseSetupController;
 use App\Controllers\Api\AdminAccountSetupController;
+use App\Controllers\Api\WebInfoSetupController;
 
 /**
  * API
@@ -23,6 +24,9 @@ $router->get('/api/installation/step1', HomeSetupController::class, 'getStructur
 $router->get('/api/installation/step2', DatabaseSetupController::class, 'getStructure');
 >>>>>>> Installeur JS : V2
 $router->post('/api/installation/createDatabase', DatabaseSetupController::class, 'createDatabase');
+
+$router->get('/api/installation/step4', WebInfoSetupController::class, 'getStructure');
+$router->post('/api/installation/setWebInfo', WebInfoSetupController::class, 'setWebInfo');
 
 $router->get('/api/installation/step3', AdminAccountSetupController::class, 'getStructure');
 $router->post('/api/installation/createUser', AdminAccountSetupController::class, 'createUser');
