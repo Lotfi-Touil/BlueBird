12c4bea552b56aa25deb738ef67ae84d53fccf58
Sohaib Mohammad <sohaib.mohammad745@gmail.com>
2023-07-20 19:42:08 +0200

    Installeur JS : Step 1

--- app/Routes/api.php
+++ app/Routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\Api\StartSetupController;
 use App\Controllers\Api\DatabaseSetupController;
 use App\Controllers\Api\AdminAccountSetupController;
 
@@ -7,7 +8,7 @@ use App\Controllers\Api\AdminAccountSetupController;
  * API
  */
 
-$router->get('/api/installation/step1', HomeSetupController::class, 'getStructure');
+$router->get('/api/installation/step1', StartSetupController::class, 'getStructure');
  
 $router->get('/api/installation/step2', DatabaseSetupController::class, 'getStructure');
 $router->post('/api/installation/createDatabase', DatabaseSetupController::class, 'createDatabase');
