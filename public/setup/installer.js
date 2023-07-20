7d059f0f15f5ac6377bd8f3bccf1dfbf2cbfbdc3
Sohaib Mohammad <sohaib.mohammad745@gmail.com>
2023-07-20 19:42:08 +0200

    Installeur JS : Step 1

--- public/setup/installer.js
+++ public/setup/installer.js
@@ -2,7 +2,8 @@ import generateStructure from './DomRenderer.js';
 
 const routes = {
   '/step1': {
-    render: renderStep1
+    render: renderStep1,
+    onSubmit: onSubmitStep1
   },
   '/step2': {
     render: renderStep2,
@@ -23,7 +24,7 @@ function startInstallation() {
   if (routes.hasOwnProperty(currentPath)) {
     routes[currentPath].render();
   } else {
-    navigateTo('/step2');
+    navigateTo('/step1');
   }
 }
 
@@ -32,7 +33,25 @@ function navigateTo(path) {
   routes[path].render();
 }
 
-function renderStep1() { }
+function renderStep1() {
+  fetch('/api/installation/step1')
+    .then(response => response.json())
+    .then(formStructure => {
+      const formElement = generateStructure(formStructure);
+      formElement.addEventListener('submit', routes['/step1'].onSubmit);
+      const appElement = document.getElementById('app');
+      while (appElement.firstChild) {
+        appElement.removeChild(appElement.firstChild);
+      }
+      appElement.appendChild(formElement);
+    });
+}
+
+function onSubmitStep1(event) {
+  event.preventDefault();
+
+  navigateTo('/step2');
+}
 
 function renderStep2() {
   fetch('/api/installation/step2')
